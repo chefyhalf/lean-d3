327e3435739c762993682c8d9fa8d723c058c23a
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-14 17:51:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/openwrt-packages/luci-app-unblockneteasemusic
+# git clone https://github.com/rosywrt/luci-theme-rosy.git package/openwrt-packages/luci-theme-rosy
+git clone https://github.com/liuran001/openwrt-packages/tree/packages/luci-theme-argon-lr package/lean/luci-theme-argon-lr
+git clone https://github.com/immortalwrt/luci/tree/master/themes/luci-theme-openwrt-2020  package/lean/luci-theme-openwrt-2020
+# git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-packages/luci-app-bypass
+sed -i '1i src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '1i src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
