e6d767a73a91ee54c80f3e50a4debd797e9f9db0
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-17 06:26:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-# git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
 # git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
 # sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
 # sed -i '1i src-git passwall https://github.com/kiddin9/openwrt-passwall' feeds.conf.default
@@ -24,9 +24,10 @@
 # git clone https://github.com/kiddin9/openwrt-bypass package/lean/luci-app-bypass
 # sed -i '1i src-git bigbighill https://github.com/bigbighill/openwrt-packages-1' feeds.conf.default
 # sed -i '1i src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
-sed -i '$a src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/OpenWrt-Actions/openwrt-package-lienol' feeds.conf.default
-# sed -i '$a src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git NueXini https://github.com/NueXini/NueXini_Packages' feeds.conf.default
+git clone https://github.com/chongteng2017/luci-app-passwall.git package/lean/luci-app-passwall
