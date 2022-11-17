1171fe7ccc8fe1a691d9ea532554ca0ab3b8a5bf
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-17 13:59:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 # sed -i '$a src-git passwall https://github.com/OpenWrt-Actions/openwrt-package-lienol' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/Never2333/openwrt-luci-app-passwall package/lean/luci-app-passwall
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# git clone https://github.com/Never2333/openwrt-luci-app-passwall package/lean/luci-app-passwall
 sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass' feeds.conf.default
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/lean/luci-app-unblockneteasemusic
 git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
