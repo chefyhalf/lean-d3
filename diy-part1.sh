bbac55b864026d94703af1b13b4c9341a1992320
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2022-11-18 10:11:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,6 +31,5 @@
 # sed -i '$a src-git liuran001 https://github.com/liuran001/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git NueXini https://github.com/NueXini/NueXini_Packages' feeds.conf.default
-# sed -i '$a src-git reducehaibo https://github.com/chefyhalf/openwrt-packages' feeds.conf.default
-sed -i '$a src-git reduceliura https://github.com/chefyhalf/liuran' feeds.conf.default
+# sed -i '$a src-git NueXini https://github.com/NueXini/NueXini_packages' feeds.conf.default
+sed -i '$a src-git reducehaibo https://github.com/chefyhalf/openwrt-packages' feeds.conf.default
